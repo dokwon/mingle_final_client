b9df0337b751205e322fcfcdaf353968a65fafda
dokwon <dokwon@stanford.edu>
2014-07-09 19:17:33 +0900

    initial commit

---
+++ mingle/src/com/example/mingle/ChatRoom.java
@@ -0,0 +1,102 @@
+package com.example.mingle;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import android.graphics.drawable.Drawable;
+
+import android.graphics.drawable.Drawable;
+
+class ChatRoom{
+	
+	private ArrayList<Message> msg_list;
+	private String recv_uid;
+	private boolean just_created;
+	private Drawable user_pic;
+	
+	public ChatRoom(String uid, Drawable image){
+		msg_list =  new ArrayList<Message>();
+		recv_uid = uid;
+		just_created = true;
+		user_pic = image;
+	}
+	
+	public boolean isJustCreated(){
+		return just_created;
+	}
+	
+	public void setChatActive(){
+		just_created = false;
+	}
+	
+	public String getRecvUid(){
+		return recv_uid;
+	}
+	
+	public void addMsg(String send_uid, Drawable image, String msg, int msg_counter, int status){
+		Date date= new Date();
+		Timestamp timestamp = (new Timestamp(date.getTime()));
+		Message msg_obj = new Message(send_uid, image, msg, msg_counter, timestamp, 0);
+		msg_list.add(msg_obj);
+		Collections.sort(msg_list, new MsgComparator());
+	}
+	
+	public void addRecvMsg(String send_uid, Drawable image, String msg, String msg_ts){
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+			Date parsedDate = dateFormat.parse(msg_ts);
+			Timestamp timestamp = new Timestamp(parsedDate.getTime());
+			
+			Message msg_obj = new Message(send_uid, image, msg, -1, timestamp, 1);
+			msg_list.add(msg_obj);
+			Collections.sort(msg_list, new MsgComparator());
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Message> getMsgList(){
+		return msg_list;
+	}
+	
+	public boolean updateMsg(String send_uid, int counter, String msg_ts){
+		for(Message obj : msg_list){
+			if(obj.getUid().equals(send_uid) && obj.getCounter()==counter){
+				obj.setStatus(1);
+        		try {
+    				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+    				Date parsedDate = dateFormat.parse(msg_ts);
+    				Timestamp timestamp = new Timestamp(parsedDate.getTime());
+    				
+    				obj.setTimestamp(timestamp);
+    				Collections.sort(msg_list, new MsgComparator());
+    			} catch (java.text.ParseException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+				return true;
+			}	
+		}
+		return false;
+	}
+	
+	public String getLastMsg(){
+		if(msg_list.size() > 0) return msg_list.get(msg_list.size() - 1).getContent();
+		return "";
+	}
+	
+	public Drawable getPic(){
+		return user_pic;
+	}
+	
+}
+
+class MsgComparator implements Comparator<Message> {
+    public int compare(Message msg1, Message msg2) {
+    	return (msg1.getTimestamp()).compareTo(msg2.getTimestamp());
+    }
+}
---
+++ mingle/src/com/example/mingle/PhotoPoster.java
@@ -0,0 +1,69 @@
+package com.example.mingle;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+
+
+public class PhotoPoster {
+	
+	public static String postPhoto(ArrayList<String> photoPaths, String UID, final String baseURL) throws Exception {
+	    
+		HttpClient client = new DefaultHttpClient();
+	    client.getParams().setParameter(CoreProtocolPNames.USER_AGENT, System.getProperty("http.agent"));
+	    
+	    HttpPost post = new HttpPost(baseURL + "add_photo");
+
+	    MultipartEntityBuilder builder = MultipartEntityBuilder.create();        
+	    builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+		
+		
+		for(int i = 0; i < photoPaths.size(); i++) { 
+			InputStream is = new FileInputStream(new File(photoPaths.get(i)));
+			byte[] data = IOUtils.toByteArray(is);
+			
+			String numString = Integer.toString(i + 1);
+			//File name format : uid_photo_# (i.e. alsvk0-124qef_photo_1.png);
+		    InputStreamBody inputStreamBody = new InputStreamBody(new ByteArrayInputStream(data),
+		    													UID+ "_photo_" + numString + ".png");
+
+		    builder.addPart("photo_" + numString, inputStreamBody);
+		}     
+		builder.addTextBody("uid", UID);
+	    final HttpEntity yourEntity = builder.build();
+
+	    post.setEntity(yourEntity);
+	    HttpResponse response = client.execute(post);        
+
+	    return getContent(response);
+	} 
+
+	public static String getContent(HttpResponse response) throws IOException {
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	    String body = "";
+	    String content = "";
+
+	    while ((body = rd.readLine()) != null) 
+	    {
+	        content += body + "\n";
+	    }
+	    return content.trim();
+	}
+
+}
